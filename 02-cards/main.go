3f2adea480452677719a9767799e4ffef3853888
Chad Blair <chadblair@gmail.com>
2017-11-14 06:22:38 -0800

    new deck

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -1,12 +1,17 @@
 package main
 
+import "fmt"
+
 // go run main.go deck.go
 
 func main()  {
 
 	cards := newDeck()
 
-	cards.print()
+	hand, remaining := deal(cards, 5)
+
+	fmt.Println(hand)
+	fmt.Println(remaining)
 }
 
 func newCard() string {
