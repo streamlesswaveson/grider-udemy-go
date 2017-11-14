6e3a1b087e5626deeaeb5e75697014b7153019c1
Chad Blair <chadblair@gmail.com>
2017-11-14 06:12:43 -0800

    new deck

--- 02-cards/deck.go
+++ 02-cards/deck.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
 // new type 'deck' extending from []string
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	suits := []string{"Spades","Diamonds", "Hearts", "Clubs"}
+	values := []string{"Ace","Two", "Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"}
+
+	for _, s := range suits{
+		for _, v := range values {
+			cards = append(cards, fmt.Sprint(v, " of ", s))
+		}
+	}
+	return cards
+}
+
+// any variable of type 'deck' now gets the print() function (receiver)
+func (d deck) print() {
+	for i, item := range d {
+		fmt.Println(i, item)
+	}
+}
--- 02-cards/main.go
+++ 02-cards/main.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
-
 // go run main.go deck.go
 
 func main()  {
 
-	cards := deck{newCard(), "Ace of Base"}
-	fmt.Println(cards)
+	cards := newDeck()
+
+	cards.print()
 }
 
 func newCard() string {
